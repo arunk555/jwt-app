c26f2220c695cdc01693f968ecbc8ab76256544d
Arun K <arunkurusev@gmail.com>
2022-06-17 21:22:22 +0530

    latest changes

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const server = http.createServer(app);
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-server.listen(port, () => {
+/*server.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});*/
+
+const servertest = server.listen(process.env.PORT || 5000, () => {
+  const port = servertest.address().port;
+  console.log(`Express is working on port ${port}`);
 });
