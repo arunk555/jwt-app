c82bdaa834c90498afb7b0dcf7eb88d580c04440
Arun K <arunkurusev@gmail.com>
2022-06-18 11:50:29 +0530

    latest changes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const http = require("http");
 const app = require("./app");
-
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
