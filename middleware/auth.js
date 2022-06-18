01580ee92a58ffc72c26334611de14a2d49f8ee8
Arun K <arunkurusev@gmail.com>
2022-06-18 11:24:37 +0530

    latest changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,7 @@ const jwt = require("jsonwebtoken");
 const TOKEN_KEY="MY_SECRET_KEY";
 
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+  const token = req.body.token || req.query.token || req.params.token || req.headers["x-access-token"];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   first_name: { type: String, default: null },
@@ -8,4 +8,4 @@ const userSchema = new mongoose.Schema({
   token: { type: String },
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = model("user", userSchema);
