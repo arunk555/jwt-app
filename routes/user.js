b59efa1c1a97bf235458ec859ede5a5572208cdd
Arun K <arunkurusev@gmail.com>
2022-06-19 19:51:58 +0530

    latest changes

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+//const auth = require("../middleware/auth");
 const {registerctrl, loginctrl, welcomectrl} = require("../controllers/user");
 
 router.post("/register", registerctrl);
 router.post("/login", loginctrl);
-router.get("/welcome", auth, welcomectrl);
 
 
 
