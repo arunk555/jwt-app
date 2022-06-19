0a666bb1b432942cf68bd55b0ccc4fea098087c7
Arun K <arunkurusev@gmail.com>
2022-06-19 10:53:42 +0530

    latest changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   //const token = req.body.token || req.query.token || req.params.token || req.headers["x-access-token"];
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjJhZWE5MjVmMWVmNTEwMDE2MDY0NzFlIiwiZW1haWwiOiJ0ZXN0dG9rZXd3QGdtYWlsLmNvbSIsImlhdCI6MTY1NTYxNDkwOCwiZXhwIjoxNjU1NjIyMTA4fQ.q8z8ZXAQEmZE2bMUUK-gKAiNKctItKw0eoOv9eTD9aY';
+  const token = req.headers["Authorization"].split(" ")[1];
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
