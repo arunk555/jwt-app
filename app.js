afc5a57b3980d2893f7def56632c77434a7f4da6
Arun K <arunkurusev@gmail.com>
2022-06-24 19:54:31 +0530

    latest changes

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const user_routes = require("./routes/user");
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use("/", user_routes);
 
 // This should be the last route else any after it won't work
