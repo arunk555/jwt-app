8b0468deea3251154494c9f24395e9bd620d41bf
Arun K <arunkurusev@gmail.com>
2022-06-24 20:36:38 +0530

    latest changes

--- model/user.js
+++ model/user.js
@@ -1,11 +1,16 @@
 const { Schema, model } = require("mongoose");
 
+const addressSchema = new Schema({
+  street: { type: String, default: null },
+  city: { type: String, default: null },
+});
+
 const userSchema = new Schema({
   first_name: { type: String, default: null },
   last_name: { type: String, default: null },
   email: { type: String, required:true, lowercase:true, unique: true },
   password: { type: String, required:true },
-  token: { type: String },
+  address: addressSchema,
   createdAt:{
     type:Date,
     immutable:true,
@@ -18,4 +23,29 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.sayHi=function(){
+  console.log(`Hi, My name is ${this.first_name}`);
+}
+
+userSchema.statics.findByName=function(){
+ return this.find({name:new RegExp(name,"i")});
+}
+
+userSchema.query.byName=function(){
+ return this.where({name:new RegExp(name,"i")});
+}
+
+userSchema.virtual('namedEmail').get(function(){
+ return `${this.first_name} <${this.email}>`;
+});
+/* Middleware inside the mongoose pre or post*/
+userSchema.pre('save',function(next){
+ this.updatedAt=Date.now();
+ next();
+});
+
+userSchema.post('save',function(doc, next){
+ doc.sayHi();
+ next();
+});
 module.exports = model("user", userSchema);
